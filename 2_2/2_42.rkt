ab96eda5f376e251d3693e5403aae154f729a96a
Ryo <ryo@unentai.jp>
2017-08-22 11:03:37 +0900

    2_42.rkt

--- 2_2/2_42.rkt
+++ 2_2/2_42.rkt
@@ -1,7 +1,15 @@
 #lang racket
 (require racket/trace)
 (require sicp)
-; 9:39->
+; 9:39->10:42
+; 10:49->11:03
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
 
 (define (enumerate-interval low high)
   (if (> low high)
@@ -21,9 +29,65 @@
 
 (define empty-board nil)
 
-;(define (safe? k positions)
+(define (adjoin-position new-row k rest)
+  (append rest (list new-row)))
 
-  ;)
+(define (index lst)
+  (define (index-iter i lst)
+    (if (null? lst)
+         nil
+         (cons (cons i (car lst)) (index-iter (+ i 1) (cdr lst))))
+
+    )
+  (index-iter 1 lst)
+  )
+(display (index '(1 4)))
+(newline)
+(define (ret_last lst)
+  (if (null? (cdr lst))
+    lst
+    (ret_last (cdr lst)))
+  )
+(define (ret_not_last lst)
+  (if (null? (cdr lst))
+      nil
+    (cons (car lst) (ret_not_last (cdr lst)))
+    )
+  )
+
+(define (_safe? k positions)
+(cond
+  ((null? (cdr positions)) true)
+  (else
+  (let ((xy_s (index positions)))
+    (let ((xy_s_p (ret_not_last xy_s)) (xy_s_k (ret_last xy_s)))
+      (let ((x-y (- (caar xy_s_k) (cdar xy_s_k)))
+            (x+y (+ (caar xy_s_k) (cdar xy_s_k)))
+            (x-y_p (- (caar xy_s_p) (cdar xy_s_p)))
+            (x+y_p (+ (caar xy_s_p) (cdar xy_s_p))))
+             (and (not (equal? x-y x-y_p)) (not (equal? x+y x+y_p))
+                  (_safe? k (cdr positions)))
+             )
+            )
+        )
+      )
+  )
+)
+(define (is_duplicate_none lst)
+  (define (is_not_duplicate a lst)
+    (cond ((null? lst) true)
+      (else (and (not (equal? a (car lst))) (is_not_duplicate a (cdr lst))))
+      )
+    )
+  (cond
+    ((null? lst) true)
+    (else
+      (and (is_not_duplicate (car lst) (cdr lst)) (is_duplicate_none (cdr lst)))
+     )
+    )
+  )
+(define (safe? k positions)
+  (and (is_duplicate_none positions) (_safe? k positions)))
 
 (define (queens board-size)
   (define (queen-cols k)
@@ -39,3 +103,4 @@
                      (enumerate-interval 1 board-size)))
         (queen-cols (- k 1))))))
   (queen-cols board-size))
+(display (length (queens 8)))
