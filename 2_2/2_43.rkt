51ba79bb19db1c0e96ebd41c99866a0f454f8250
Ryo <ryo@unentai.jp>
2017-08-22 12:20:06 +0900

    2_43.rkt

--- 2_2/2_43.rkt
+++ 2_2/2_43.rkt
@@ -5,7 +5,8 @@
 ; 10:49->11:03
 
 ;2.43
-;11:08->
+;11:08->11:50
+;12:00->
 
 (define (filter predicate sequence)
   (cond ((null? sequence) nil)
@@ -106,4 +107,39 @@
                      (enumerate-interval 1 board-size)))
         (queen-cols (- k 1))))))
   (queen-cols board-size))
-(display (length (queens 9)))
+
+(define start1 (runtime))
+(display (length (queens 8)))
+(newline)
+(display (- (runtime) start1))
+
+(define (queens2 board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+	  (lambda (new-row)
+	    (map (lambda (rest-of-queens)
+		   (adjoin-position new-row k rest-of-queens))
+		 (queen-cols (- k 1))))
+	  (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+(newline)
+(define start2 (runtime))
+(display (length (queens2 8)))
+(newline)
+(display (- (runtime) start2))
+
+;通常版→queen-cols*8
+;失敗版→queen-cols呼び出し 8**7
+;オーダーは8**6*T？
+;(失敗版の計算量)
+;8*(queen-cols 7)
+;= 8* 8* (queen-cols 6)
+;=…
+;=8**8*(queen-cols 0)
+;(通常版の計算量)
+;= 8**2(queen-cols 0)
