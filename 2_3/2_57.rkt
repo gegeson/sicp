630d1dc359182e8165298df3b1bd919ad10056e9
Ryo <ryo@unentai.jp>
2017-09-05 12:32:36 +0900

    2.57和　今度こそ成功

--- 2_3/2_57.rkt
+++ 2_3/2_57.rkt
@@ -3,7 +3,8 @@
 (require racket/trace)
 ; 21:47->22:19
 ; 22:24->0:06
-; 10:30->
+; 10:30->11:08
+; 11:27->12:06
 (define (square n)
   (* n n))
 (define (even? n)
@@ -16,6 +17,7 @@
 
 
 (define (deriv exp var)
+  (display "exp is ")(display exp)(newline)
   (cond ((number? exp) 0)
         ((variable? exp)
          (if (same-variable? exp var) 1 0))
@@ -53,7 +55,11 @@
 
 (define (addend s) (cadr s))
 
-(define (augend s) (caddr s))
+(define (augend s)
+  (if (< (length (cddr s)) 2)
+      (caddr s)
+      (cons '+ (cddr s))))
+
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
@@ -82,6 +88,62 @@
 ;        ((and (number? a1) (number? a2)) (+ a1 a2))
 ;        (else (list '+ a1 a2))))
 
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (elim_zero lst)
+    (cond
+      ((null? lst) nil)
+      ((equal? (car lst) 0) (elim_zero (cdr lst)))
+      (else
+        (cons (car lst) (elim_zero (cdr lst)))
+       )
+      )
+  )
+
+
+(define (make-sum a . a_)
+  (display "a is ")(display a)(newline)
+  (display "a_ is ")(display a_)(newline)
+  (cond
+      ((=number? a 0) (car a_))
+      ((=number? (car a_) 0) a)
+    ((and (number? a) (number? a_) (+ a (car a_))))
+    (else (list '+ a (car a_)))
+    )
+  )
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponentiation base exp)
+  (cond ((=number? exp 0) 1)
+        ((=number? exp 1) base)
+        ((and (number? base) (number? exp)) (fast-expt base exp))
+    (else (list '** base exp))
+    )
+  )
+;(trace deriv)
+;(trace make-sum)
+;(display (deriv '(** x 5) 'x))
+;(newline)
+(display (deriv '(+ (+ (** x 2) x)  5) 'x))
+(newline)
+(display (deriv '(+ u (* x 5) y z) 'x))
+(newline)
+(display (deriv '(+ (** x 4) (** x 2)  5) 'x))
+(newline)
+(display (deriv '(+ y z (** x 4) u (** x 3) v (** x 2)  5) 'x))
+(newline)
+(display (deriv '(+ (* 4 (** x 3)) (+ (* 3 (** x 2)) (* 2 x))) 'x))
+
+
+;ゴミ置き場（失敗作）
 ;(define (make-sum . a_)
 ;    (cond
 ;      ((null? a_) nil)
@@ -101,75 +163,24 @@
 ;      )
 ;    )
 ;  )
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
-(define (elim_zero lst)
-    (cond
-      ((null? lst) nil)
-      ((equal? (car lst) 0) (elim_zero (cdr lst)))
-      (else
-        (cons (car lst) (elim_zero (cdr lst)))
-       )
-      )
-  )
-
-(define (make-sum . a_)
-  (define (make-sum-iter a_ sum)
-    (cond
-      ((= (length a_) 1) (car a_))
-      ((and (null? (cdr a_)) (number? (car a_)))
-        (+ sum (car a_)))
-      ((number? (car a_))
-       (make-sum-iter (cdr a_) (+ (car a_) sum)))
-      (else
-       (if (= sum 0)
-         (cons '+ a_ )
-         (list '+ sum a_)) ;後半部分が0になるとき省略すること
-       )
-      ))
-  (make-sum-iter (elim_zero a_) 0)
-  )
-
-(define (make-sum-iter a_ sum)
-  (cond
-    ((and (null? (cdr a_)) (number? (car a_)))
-        (+ sum (car a_)))
-    ((number? (car a_))
-     (make-sum-iter (cdr a_) (+ (car a_) sum)))
-    (else
-     (if (= sum 0)
-       (list a_)
-       (list '+ sum  a_))
-     )
-    ))
-
-(display (make-sum-iter '(1 2 (+ 5 6) 0) 5))
-(newline)
-(display (make-sum 1 2 3 0 4 5 0))
-
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-
-(define (make-exponentiation base exp)
-  (cond ((=number? exp 0) 1)
-        ((=number? exp 1) base)
-        ((and (number? base) (number? exp)) (fast-expt base exp))
-    (else (list '** base exp))
-    )
-  )
-;(trace deriv)
-;(trace make-sum)
-(display (deriv '(** x 5) 'x))
-(newline)
-(display (deriv '(+ (+ (** x 2) x)  5) 'x))
-(newline)
-(display (deriv '(+ (* 3 x) y z) 'x))
-(newline)
-(display (deriv '(+ (** x 4) (** x 2)  5) 'x))
-(newline)
+;
+;(define (make-sum . a_)
+;  (define (make-sum-iter a_ sum)
+;  ;(display "a_ is ") (display a_)(newline)
+;    (cond
+;      ((null? a_) nil)
+;      ((= (length a_) 1) (car a_))
+;      ((and (null? (cdr a_)) (number? (car a_)))
+;        (+ sum (car a_)))
+;      ((number? (car a_))
+;       (make-sum-iter (cdr a_) (+ (car a_) sum)))
+;      (else
+;       (if (= sum 0)
+;         (cons '+ a_ )
+;         (list '+ sum a_))
+;       )
+;      ))
+;  ;(display "ビフォー a_ ") (display a_) (newline)
+;  ;(display "ここからmake-sum-iter ")(newline)
+;   (make-sum-iter  (elim_zero a_) 0)
+;  )
