9ec6d84d6c5e8e22babcbf6660a51e8b89e77587
Ryo <ryo@unentai.jp>
2017-10-14 14:06:18 +0900

    線形リストだけおかしくなるバグ

--- 3_3/3.17.re.rkt
+++ 3_3/3.17.re.rkt
@@ -3,13 +3,21 @@
 (require racket/trace)
 ;12:06->13:39
 ;3ポモドーロ
-;13:39->
+;13:39->14:05
+;+1ポモドーロ
 ;以下のa5で3にならず2になる理由にずっと悩んでいたが、やっと判明。
 ;最初の時点でリストに加えてしまうせいで、
 ;一番下でもう一度出てきた時にもう遭遇済みと判定している。
 ;そのせいで、一番上と二段目しかリストを探索せず、
 ;しかもそれぞれ+1としかしていないから2が出力される。
 ;うまくいかない理由がわかったので、改善案を考える。
+;改善verがcount-pairs3である。
+;こんどはa7が3になるべきなのに4になってこける。
+;原因判明。そして完成。
+;条件節を
+;(not (has (car x) lst))
+;というふうにしたら行けた。よく考えたらこれと逆とは排他的でない。
+;cdrを持つがcarも持つ、という事がありうるのだ。
 
 (define (count-pairs1 x)
   (if (not (pair? x))
@@ -26,33 +34,27 @@
   )
 (define (count-pairs3 x)
   (define lst nil)
-  (define (num-pair x)
-    (cond
-    [(and (pair? x) (pair? (car x)) (pair? (cdr x))) 2]
-    [(and (pair? x) (not (pair? (car x))) (not (pair? (cdr x)))) 0]
-    [else 1]
-      ))
     (define (sub x)
       (cond
         [(not (pair? x)) 0]
         [(and (has (car x) lst) (has (cdr x) lst))
          0]
-         [(has (cdr x) lst)
+         [(not (has (car x) lst))
           (begin (set! lst (cons (car x) lst))
                 ;(printf "lst ~a \n " lst)
                  (+ 1 (sub (car x))))]
-        [(has (car x) lst)
+        [(not (has (cdr x) lst))
          (begin (set! lst (cons (cdr x) lst))
                 ;(printf "lst ~a \n" lst)
                 (+ 1 (sub (cdr x))))]
         [(eq? (car x) (cdr x))
          (begin (set! lst (cons (car x) lst))
-                (+ (sub (car x)) 1))
+                (+ (sub (car x)) (sub (cdr x)) 1))
          ]
         [else
           (begin (set! lst (cons (car x) (cons (cdr x) lst)))
           ; (printf "lst ~a \n" lst)
-           (+ (sub (car x)) (sub (cdr x)) 2)]
+           (+ (sub (car x)) (sub (cdr x)) 2))]
         ))
     (sub x)
     )
@@ -75,3 +77,7 @@
 (define a7 (cons a6 a1))
 (count-pairs1 a7)
 (count-pairs3 a7)
+(newline)
+(define a8 (cons 'a (cons 'b (cons 'c nil))))
+(count-pairs1 a8)
+(count-pairs3 a8)
