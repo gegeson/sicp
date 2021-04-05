d33beebe45b87c2924877b382f863ff29e0fff27
Ryo Yoshida <nekonekosekai@gmail.com>
2021-04-05 16:15:43 +0900

    2.41

--- 2_2/2.41.rkt
+++ 2_2/2.41.rkt
@@ -27,13 +27,27 @@
   (flatmap (lambda (i)
     (flatmap (lambda (j)
       (map (lambda (k)
-        (list i j k)
+        (list k j i)
           )
         (enumerate-interval 1 (- j 1))))
       (enumerate-interval 1 (- i 1))))
     (enumerate-interval 1 n)))
-(display (unique-trines 5))
-
+(display (unique-trines 3))
+;;; for i in 1..n:
+;;;   for j in 1..i-1:
+;;;     for k in 1..j-1:
+;;;       print(k, j, i)
+;;; iが1~5
+;;; jは1~4
+;;; kは1~3
+;;; iが1->i-1=0なので、ループが回らない
+;;; jが1->j-1=0なので、ループが回らない
+;;; つまり、
+;;; 最低でもiが3, jが2の時初めてループが回る
+;;; iが3なら、jは1か2で、1は除外で2なので、kは1
+;;; このパターンだけが許容されるので、(1, 2, 3)が出力される
+;;; iが4なら、jは2か3で、2の時(1, 2, 3)
+;;; 3の時、kは1か2なので、(1, 3, 4)と(2, 3, 4)が追加されて出力される
 (define (filter predicate sequence)
   (cond ((null? sequence) nil)
         ((predicate (car sequence))
